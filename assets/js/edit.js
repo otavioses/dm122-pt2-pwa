358d71583b426dbb7f40432f1f4def232c561de4
Otávio Souza <otavioses>
2020-12-13 16:34:29 -0300

    Register service worker only at index

--- assets/js/edit.js
+++ assets/js/edit.js
@@ -3,24 +3,11 @@ import ContactService from "./ContactService.js";
 
 class Edit {
   constructor() {
-    this.registerServiceWorker();
     this.initialize();
   }
 
   initialize() {
     new EditService(new ContactService());
-  }
-
-  registerServiceWorker() {
-    if ("serviceWorker" in navigator) {
-      const onsuccess = () => console.log("[Service Worker] Registered");
-      const onfailure = () => console.log("[Service Worker] Failed");
-
-      navigator.serviceWorker
-        .register("sw.js")
-        .then(onsuccess)
-        .catch(onfailure);
-    }
   }
 }
 
--- assets/js/register.js
+++ assets/js/register.js
@@ -3,24 +3,11 @@ import ContactService from "./ContactService.js";
 
 class Register {
   constructor() {
-    this.registerServiceWorker();
     this.initialize();
   }
 
   initialize() {
     new RegisterService(new ContactService());
-  }
-
-  registerServiceWorker() {
-    if ("serviceWorker" in navigator) {
-      const onsuccess = () => console.log("[Service Worker] Registered");
-      const onfailure = () => console.log("[Service Worker] Failed");
-
-      navigator.serviceWorker
-        .register("sw.js")
-        .then(onsuccess)
-        .catch(onfailure);
-    }
   }
 }
 
