a94926b3daa6a85c1803cfed71e9a675931ac19a
Otávio Souza <otavioses>
2020-12-13 14:45:42 -0300

    Fix edit path

--- assets/js/HtmlService.js
+++ assets/js/HtmlService.js
@@ -16,7 +16,7 @@ export default class HtmlService {
   }
 
   async editContact(contact) {
-    document.location.href = 'edit.html?contactId='+contact.id;
+    document.location.href = '/edit.html?contactId='+contact.id;
   }
 
   addToHtmlList(contact) {
